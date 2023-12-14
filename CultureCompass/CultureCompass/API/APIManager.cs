79ba3fe29c59f406c7125f37306299c1f3cd00d2
NielsB1 <n.buijs@student.avans.nl>
2023-12-14 15:40:49 +0100

    code klopt, maar API key heeft niet de juiste rechten

---
+++ CultureCompass/CultureCompass/API/APIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using CultureCompass.Navigation;
+using Newtonsoft.Json;
+
+namespace CultureCompass.API
+{
+    internal class APIManager
+    {
+        public async Task<APIResponse> WriteData(string origin, string destination)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Data data = new Data(origin, destination);
+                var fullUrl = "https://maps.googleapis.com/maps/api/directions/json" + data.GetData();
+
+                HttpResponseMessage response = await httpClient.GetAsync(fullUrl);
+                response.EnsureSuccessStatusCode();
+
+                string jsonContent = await response.Content.ReadAsStringAsync();
+                APIResponse responseData = JsonConvert.DeserializeObject<APIResponse>(jsonContent);
+
+                Trace.WriteLine(jsonContent);
+                return responseData;
+            }
+        }
+    }
+
+    internal class Data(string origin, string destination)
+    {
+        const string apiKey = "AIzaSyBXG_XrA3JRTL58osjxd0DbqH563e2t84o";
+
+        public string GetData()
+        {
+            return $"?destination={destination}&origin={origin}&key={apiKey}&mode=bicycling";
+        }
+    }
+}
---
+++ CultureCompass/CultureCompass/Navigation/RouteLine.cs
@@ -0,0 +1,33 @@
+﻿using CultureCompass.API;
+using Location = Microsoft.Maui.Devices.Sensors.Location;
+using Polyline = Microsoft.Maui.Controls.Maps.Polyline;
+
+namespace CultureCompass.Navigation
+{
+    internal class RouteLine
+    {
+        private APIManager apiManager = new();
+        public async Task<Polyline> GetRouteLine(string origin, string destination)
+        {
+            APIResponse result = await apiManager.WriteData(origin, destination);
+
+            Polyline polyline = new Polyline()
+            {
+                StrokeColor = Colors.Blue,
+                StrokeWidth = 12
+            };
+
+            foreach (Route route in result.Routes)
+            {
+                foreach (Leg leg in route.Legs)
+                {
+                    foreach (Step step in leg.Steps)
+                    {
+                        polyline.Add(new Location(step.StartLocation.Lat, step.StartLocation.Lng));
+                    }
+                }
+            }
+            return polyline;
+        }
+    }
+}
