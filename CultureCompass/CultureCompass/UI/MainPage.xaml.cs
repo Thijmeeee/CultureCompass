6d2f25494702e5208c65a92da5157933f6d60c89
NielsB1 <n.buijs@student.avans.nl>
2023-12-19 13:07:11 +0100

    cleaned up code, and added check for loading database

--- CultureCompass/CultureCompass/UI/MainPage.xaml.cs
+++ CultureCompass/CultureCompass/UI/MainPage.xaml.cs
@@ -9,7 +9,7 @@ namespace CultureCompass.UI
 {
     public partial class MainPage : ContentPage
     {
-        private Database.Database _database;
+        private DatabaseManager _database;
         public MainPage()
         {
             InitializeComponent();
@@ -17,7 +17,9 @@ namespace CultureCompass.UI
             //RouteManager routeManager = new RouteManager(new NavigationPage());
             //routeManager.SetRoute(new Route());
 
-           // LoadWaypointsIntoDatabase();
+            _database = new DatabaseManager();
+            if (_database.GetAllWaypoints().Count == 0)
+                LoadWaypointsIntoDatabase();
 
             // TestDatabase();
         }
@@ -154,89 +156,17 @@ namespace CultureCompass.UI
                 InfoDutch = "Dit is een museum dat het verhaal vertelt van de bevrijding van Breda. Het is voortgekomen uit het Generaal Maczek Museum uit 2015."
             };
 
-            DatabaseManager databaseManager = new DatabaseManager();
-            databaseManager.AddWaypoint(waypoint1);
-            databaseManager.AddWaypoint(waypoint2);
-            databaseManager.AddWaypoint(waypoint3);
-            databaseManager.AddWaypoint(waypoint4);
-            databaseManager.AddWaypoint(waypoint5);
-            databaseManager.AddWaypoint(waypoint6);
-            databaseManager.AddWaypoint(waypoint7);
-            databaseManager.AddWaypoint(waypoint8);
-            databaseManager.AddWaypoint(waypoint9);
-            databaseManager.AddWaypoint(waypoint10);
+            _database.AddWaypoint(waypoint1);
+            _database.AddWaypoint(waypoint2);
+            _database.AddWaypoint(waypoint3);
+            _database.AddWaypoint(waypoint4);
+            _database.AddWaypoint(waypoint5);
+            _database.AddWaypoint(waypoint6);
+            _database.AddWaypoint(waypoint7);
+            _database.AddWaypoint(waypoint8);
+            _database.AddWaypoint(waypoint9);
+            _database.AddWaypoint(waypoint10);
         }
-
-        //private void TestDatabase()
-        //{
-        //    //create database
-        //    _database = new Database.Database();
-
-
-        //    //create waypoint
-        //    Waypoint waypoint = new Waypoint()
-        //    {
-        //        ID = 1,
-        //        Name = "test",
-        //        Year = 2,
-        //        ImagePath = "what",
-        //        InfoEnglish = "yes",
-        //        InfoDutch = "ja",
-        //        InfoFrench = "oui",
-        //        X = 1.2,
-        //        Y = 16.7
-        //    };
-
-        //    //create and save new database waypoint object
-        //    _database.CreateWaypoint(waypoint);
-
-        //    //get/read waypoint object from database by id
-        //    Waypoint received = _database.ReadWaypoint(1);
-
-        //    //make database waypoint object
-        //    WaypointTable updatedReceived = new WaypointTable()
-        //    {
-        //        WaypointId = received.ID,
-        //        Name = received.Name,
-        //        YearCreated = 6969,
-        //        PictureName = received.ImagePath,
-        //        InfoEnglish = received.InfoEnglish,
-        //        InfoDutch = received.InfoDutch,
-        //        InfoFrench = received.InfoFrench,
-        //        XCoordinate = received.X,
-        //        YCoordinate = received.Y
-        //    };
-
-        //    //update the existing waypoint in the database by giving new object
-        //    //primary key (waypointid) is the same, so will overwrite it
-        //    _database.UpdateWaypoint(updatedReceived);
-
-        //    //get/read new updated waypoint object
-        //    Waypoint newReceived = _database.ReadWaypoint(1);
-
-        //    //show properties of waypoint object in labels in app
-        //    //waypointid.Text = newReceived.ID.ToString();
-        //    //name.Text = newReceived.Name;
-        //    //yearcreated.Text = newReceived.Year.ToString();
-        //    //picturename.Text = newReceived.ImagePath;
-        //    //infoenglish.Text = newReceived.InfoEnglish;
-        //    //infodutch.Text = newReceived.InfoDutch;
-        //    //infofrench.Text = newReceived.InfoFrench;
-        //    //xcoordinate.Text = newReceived.X.ToString();
-        //    //ycoordinate.Text = newReceived.Y.ToString();
-
-        //    //xaml code:
-        //    // <Label x:Name="waypointid"></Label>
-        //    // <Label x:Name="name"></Label>
-        //    // <Label x:Name="yearcreated"></Label>
-        //    // <Label x:Name="picturename"></Label>
-        //    // <Label x:Name="infoenglish"></Label>
-        //    // <Label x:Name="infodutch"></Label>
-        //    // <Label x:Name="infofrench"></Label>
-        //    // <Label x:Name="xcoordinate"></Label>
-        //    // <Label x:Name="ycoordinate"></Label>
-
-        //}
 
         public void OnStartClicked(object sender, EventArgs e)
         {
